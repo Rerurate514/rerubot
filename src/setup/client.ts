118581e9068ee585ca9884a66537bc1bd9ebd53e
Rerurate514 <rerurateyuto@gmail.com>
2026-05-18 15:10:16 +0900

    intent更新

--- src/setup/client.ts
+++ src/setup/client.ts
@@ -4,6 +4,7 @@ export const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, 
         GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildVoiceStates
     ] 
